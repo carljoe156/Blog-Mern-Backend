a2d0fe89033b9b616195f20df65fb09a7ba7b6a0
Carl Joseph <carljoseph156@gmail.com>
2025-02-03 19:04:24 -0500

    Updated Index

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/", route);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Blog-MERN API</h1><ol> </ol>");
+});
+
+// default, catch-all route
+app.get("/*", (req, res) => {
+  res.redirect("/");
+});
+
+//Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("There was an issue on the server, Try Again!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

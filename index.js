e694eb818b42cb1bdc12fad2e29e99659f0d1f86
Carl Joseph <carljoseph156@gmail.com>
2025-02-06 21:53:33 -0500

    Updated Index

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+
 app.use("/", route);
 
 app.get("/", (req, res) => {
   res.send("<h1>Blog-MERN API</h1><ol> </ol>");
 });
+
+// our routes
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+app.use("/comments", commentRoutes);
 
 // default, catch-all route
 app.get("/*", (req, res) => {

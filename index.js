35ce344237c34d64ca0b1e04cad6f06ca105ec8a
Carl Joseph <carljoseph156@gmail.com>
2025-02-07 20:58:37 -0500

    Updated index

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connection = require("./database/db.js");
+connection();
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,7 +12,7 @@ const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 
-app.use("/", route);
+// app.use("/");
 
 app.get("/", (req, res) => {
   res.send("<h1>Blog-MERN API</h1><ol> </ol>");

5597ff516f5af877295046e5aa4a840039b2f1d3
Carl Joseph <carljoseph156@gmail.com>
2025-02-10 13:30:40 -0500

    Added/Updated User/Auth controllers, models and routes

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+  },
+
+  email: {
+    type: String,
+  },
+
+  password: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
